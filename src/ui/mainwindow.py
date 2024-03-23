f58464f92760dff4391850271300926455774a7d
redtide <redtid3@gmail.com>
2024-03-23 01:31:06 +0100

    Resources update, put icons in their own directory

--- src/ui/mainwindow.py
+++ src/ui/mainwindow.py
@@ -3,13 +3,19 @@ from PySide6.QtGui     import QIcon
 from PySide6.QtWidgets import QMainWindow
 from .ui_mainwindow    import Ui_MainWindow
 from .tabpan           import tabPanAddKnobs
-import resources.rc_resources as rc
+
+import sys, importlib.util
+spec      = importlib.util.spec_from_file_location("rc", "resources/rc_resources.py")
+resources = importlib.util.module_from_spec(spec)
+sys.modules["resources"] = resources
+spec.loader.exec_module(resources)
+import resources as rc
 
 class MainWindow(QMainWindow):
   def __init__(self, parent=None):
     super().__init__(parent)
 
-    rc.qInitResources() # TODO: Not needed, just silence the import warning above.
+    rc.qInitResources() # TODO: Not needed, just silence a warning about the import above.
 
     self.ui = Ui_MainWindow()
     self.ui.setupUi(self)
@@ -19,6 +25,13 @@ class MainWindow(QMainWindow):
     self.ui.actSave.setIcon(QIcon.fromTheme("document-save",      QIcon(":/document-save")))
     self.ui.actSaveAs.setIcon(QIcon.fromTheme("document-save-as", QIcon(":/document-save-as")))
     self.ui.actQuit.setIcon(QIcon.fromTheme("application-exit",   QIcon(":/application-exit")))
+
+    self.ui.pbnMapAdd.setIcon(QIcon.fromTheme("list-add",            QIcon(":/list-add")))
+    self.ui.pbnMapClone.setIcon(QIcon.fromTheme("edit-copy",         QIcon(":/edit-copy")))
+    self.ui.pbnMapDelete.setIcon(QIcon.fromTheme("list-remove",      QIcon(":/list-remove")))
+    self.ui.pbnMapDown.setIcon(QIcon.fromTheme("go-down",            QIcon(":/go-down")))
+    self.ui.pbnMapImport.setIcon(QIcon.fromTheme("emblem-downloads", QIcon(":/import")))
+    self.ui.pbnMapUp.setIcon(QIcon.fromTheme("go-up",                QIcon(":/go-up")))
 
     self.ui.pbnAmpEnvAttackShapeEnable.clicked.connect(self.onAmpEnvAttackShapeEnabled)
     self.ui.pbnAmpEnvDecayShapeEnable.clicked.connect(self.onAmpEnvDecayShapeEnabled)

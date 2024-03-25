1172fd133b95366a1c4e235ff8b60494f5b161ef
redtide <redtid3@gmail.com>
2024-03-25 15:03:43 +0100

    Added key bindings for VSCode
    open UI in QtDesigner (Ctrl+Super+D) and compile the modified UI files (Ctrl+Super+U).
    See the comment in .vscode/keybindings.json

--- scripts/designer.sh
+++ scripts/designer.sh
@@ -2,4 +2,4 @@
 
 APP_PATH="$(dirname -- "${BASH_SOURCE[0]}")"
 export LD_PRELOAD=libpython3.11.so
-PYSIDE_DESIGNER_PLUGINS="$APP_PATH/scripts" designer6 "${@}"
+PYSIDE_DESIGNER_PLUGINS="$APP_PATH" designer6 "$APP_PATH/../src/ui/mainwindow.ui"
